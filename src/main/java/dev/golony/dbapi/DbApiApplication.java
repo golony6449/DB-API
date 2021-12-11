2fdd5e6e4347675b8182061c5cf6c59e58e6a439
Parkseongheum <ab6449@gmail.com>
2021-12-11 23:11:06 +0900

    HealthCheck url 작성

--- src/main/java/dev/golony/dbapi/DbApiApplication.java
+++ src/main/java/dev/golony/dbapi/DbApiApplication.java
@@ -2,12 +2,19 @@ package dev.golony.dbapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DbApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DbApiApplication.class, args);
     }
 
+    @GetMapping("/ping")
+    public String ping(){
+        return "pong";
+    }
 }
